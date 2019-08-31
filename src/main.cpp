263a858a9ba4a474c16fd0cbfe0ee51e715ab03b
Nitish Puri <npuri1903@gmail.com>
2019-08-31 22:08:44 +0530

    move line drawing to utils.

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,17 @@
 #include "tgaimage.h"
 #include "utils.h"
 
-namespace Colors{
-constexpr TGAColor White(255, 255, 255, 255);
-constexpr TGAColor Red(255, 0, 0, 255);
-}
-
-// Draw line segments.
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-  loop(x, x0, x1+1, 1) {  
-    auto t = (x - x0) / double(x1 - x0);
-    auto y = static_cast<int>(y0 + (y1 - y0) * t);
-    image.set(x, y, color);
-  }
-}
-
 int main() {
   TGAImage image(800, 800, TGAImage::RGB);
   image.set(52, 41, Colors::Red);
   image.flip_vertically(); // i want to have the origin at the left bottom
                            // corner of the image
 
-  // make a line
-  line(10, 10, 600, 600, image, Colors::Red);
-  line(500, 10, 600, 700, image, Colors::White);
+  for (auto i = 0; i < 1000000; i++) {
+    line(10, 10, 600, 600, image, Colors::Red);
+    line(500, 10, 600, 700, image, Colors::White);
+  }
+
   image.write_tga_file("output.tga");
   return 0;
 }
--- src/utils.h
+++ src/utils.h
@@ -1,4 +1,48 @@
 #pragma once
+#include "tgaimage.h"
 
+namespace Colors {
+constexpr TGAColor White(255, 255, 255, 255);
+constexpr TGAColor Red(255, 0, 0, 255);
+} // namespace Colors
+
+
+// Draw line segments.
+// https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+//
+void line(int x0, int y0, int x1, int y1, TGAImage &image,
+          const TGAColor &color) {
+  auto steep = false;
+  // transpose in case the line is steep.
+  if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
+    std::swap(x0, y0);
+    std::swap(x1, y1);
+    steep = true;
+  }
+  // start from the lower x position.
+  if (x0 > x1) {
+    std::swap(x0, x1);
+    std::swap(y0, y1);
+  }
+  int dx = (x1 - x0);
+  int dy = (y1 - y0);
+  double derror = std::abs(dy / double(dx));
+  double error = 0;
+  int y = y0;
+  for (auto x = x0; x <= x1; x++) {
+    if (steep) {
+      image.set(y, x, color);
+    } else {
+      image.set(x, y, color);
+    }
+    error += derror;
+    if (error > .5) {
+      y += (y1 > y0 ? 1 : -1);
+      error -= 1;
+    }
+  }
+}
+
+// MACROS
 #define loop(variable, initial, final, incr) \
   for(auto variable = initial; variable < final; variable+=incr)
