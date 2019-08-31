cb13a3219018afb35b5c17d97591ef2bca641813
Nitish Puri <npuri1903@gmail.com>
2019-08-31 17:18:26 +0530

    Line method version 2.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 
 #include "tgaimage.h"
+#include "utils.h"
 
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red = TGAColor(255, 0, 0, 255);
+namespace Colors{
+constexpr TGAColor White(255, 255, 255, 255);
+constexpr TGAColor Red(255, 0, 0, 255);
+}
 
 // Draw line segments.
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-  for (auto t = 0.; t < 1; t += .01) {
-    auto x = static_cast<int>(x0 + (x1 - x0) * t);
+  loop(x, x0, x1+1, 1) {  
+    auto t = (x - x0) / double(x1 - x0);
     auto y = static_cast<int>(y0 + (y1 - y0) * t);
     image.set(x, y, color);
   }
@@ -15,12 +18,13 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 
 int main() {
   TGAImage image(800, 800, TGAImage::RGB);
-  image.set(52, 41, red);
+  image.set(52, 41, Colors::Red);
   image.flip_vertically(); // i want to have the origin at the left bottom
                            // corner of the image
 
   // make a line
-  line(10, 10, 600, 600, image, red);
+  line(10, 10, 600, 600, image, Colors::Red);
+  line(500, 10, 600, 700, image, Colors::White);
   image.write_tga_file("output.tga");
   return 0;
 }
---
+++ src/utils.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define loop(variable, initial, final, incr) \
+  for(auto variable = initial; variable < final; variable+=incr)
