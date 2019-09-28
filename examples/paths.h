fe395baf34641be123e73b37bf6971b3d2c754e2
Nitish Puri <npuri1903@gmail.com>
2019-09-28 19:30:45 +0530

    move to jpg

--- examples/paths.h
+++ examples/paths.h
@@ -12,10 +12,14 @@ std::string GetObjPath(std::string model) {
 }
 
 std::string GetDiffuseTexture(std::string model) {
-  return BASE_DIR + "obj/" + model + "/" + model + "_diffuse.bmp";
+  return BASE_DIR + "obj/" + model + "/" + model + "_diffuse.jpg";
+}
+
+std::string GetNormalTexture(std::string model) {
+  return BASE_DIR + "obj/" + model + "/" + model + "_nm.jpg";
 }
 
 std::string GetOutputPath(const std::string model, const std::string proj,
                           const std::string suffix) {
-  return BASE_DIR + "out/" + model + "/" + proj + suffix + ".bmp";
+  return BASE_DIR + "out/" + model + "/" + proj + suffix + ".jpg";
 }
