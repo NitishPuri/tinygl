17f9c402770ccafb7ff7b4d821f706da4c8d9bc4
Nitish Puri <npuri1903@gmail.com>
2019-09-28 19:32:07 +0530

    Add normal mapping example.

--- examples/8_normal_mapping.cpp
+++ examples/8_normal_mapping.cpp
@@ -8,78 +8,11 @@
 #include "paths.h"
 
 const auto MODEL = MODELS[0];
-const auto PROJ_NO = "07_";
+const auto PROJ_NO = "08_";
 
 constexpr int width = 800;
 constexpr int height = 800;
 constexpr int depth = 10;
-
-struct NoShader : IShader {
-  NoShader() { _name = "NoShader"; }
-  Model *model;
-
-  // uniform variables
-  Color u_color;
-  Matrix u_mvp; // model view projection matrix
-
-  Vec3f vertex(int face_idx, int v_idx) override {
-    auto v = model->vert(model->face(face_idx)[v_idx].v_idx);
-    return utils::m2v(u_mvp * utils::v2m(v));
-  }
-  bool fragment(Vec3f, Color &color) override {
-    color = u_color;
-    return true;
-  }
-};
-
-struct WireframeShader : IShader {
-  WireframeShader() { _name = "WireframeShader"; }
-  Model *model;
-
-  // uniform variables
-  Color u_color;
-  Matrix u_mvp; // model view projection matrix
-
-  Vec3f vertex(int face_idx, int v_idx) override {
-    auto v = model->vert(model->face(face_idx)[v_idx].v_idx);
-    return utils::m2v(u_mvp * utils::v2m(v));
-  }
-  bool fragment(Vec3f bc, Color &color) override {
-    constexpr auto eps = 0.3f;
-    if (bc.x() < eps || bc.y() < eps || bc.z() < eps) {
-      return false;
-    }
-    color = u_color;
-    return true;
-  }
-};
-
-struct FlatShader : IShader {
-  FlatShader() { _name = "FlatShader"; }
-  // vertex data
-  const Model *model;
-
-  // varying
-  float v_intensity;
-
-  // uniform variables
-  Vec3f u_lightDir;
-  Color u_color;
-  Matrix u_mvp; // model view projection matrix
-
-  Vec3f vertex(int face_idx, int v_idx) override {
-    const auto &f = model->face(face_idx);
-    auto v = model->vert(f[v_idx].v_idx);
-    auto n = model->face_normal(face_idx);
-    v_intensity = std::clamp(n * u_lightDir, 0.f, 1.f);
-    return utils::m2v(u_mvp * utils::v2m(v));
-  }
-  bool fragment(Vec3f, Color &color) override {
-    // auto intensity = (u_lightDir[0] + u_lightDir[1] + u_lightDir[2]) / 3.f;
-    color = u_color * v_intensity;
-    return true;
-  }
-};
 
 struct GouraudShader : IShader {
   GouraudShader() { _name = "GouraudShader"; }
@@ -108,142 +41,55 @@ struct GouraudShader : IShader {
   }
 };
 
-struct SmoothShader : IShader {
-  SmoothShader() { _name = "SmoothShader"; }
-  // vertex data
-  const Model *model;
-
-  // varying
-  Vec3f v_intensity;
-
-  // uniform variables
-  Vec3f u_lightDir;
-  Matrix u_mvp; // model view projection matrix
-
-  Vec3f vertex(int face_idx, int v_idx) override {
-    const auto &f = model->face(face_idx);
-    auto v = model->vert(f[v_idx].v_idx);
-    auto n = model->normal(f[v_idx].n_idx);
-    v_intensity[v_idx] = std::max(0.f, n * u_lightDir * -1.f);
-    return utils::m2v(u_mvp * utils::v2m(v));
-  }
-  bool fragment(Vec3f bc, Color &color) override {
-    auto intensity = v_intensity * bc;
-    color = Color(unsigned char(intensity * 255),
-                  unsigned char((1 - intensity) * intensity * 255),
-                  unsigned char(intensity * 255));
-    return true;
-  }
-};
-
-struct ToonShader : IShader {
-  ToonShader() { _name = "ToonShader"; }
-  // vertex data
-  const Model *model;
-
-  // varying
-  Vec3f v_intensity;
-
-  // uniform variables
-  Vec3f u_lightDir;
-  Matrix u_mvp; // model view projection matrix
-
-  Vec3f vertex(int face_idx, int v_idx) override {
-    const auto &f = model->face(face_idx);
-    auto v = model->vert(f[v_idx].v_idx);
-    auto n = model->normal(f[v_idx].n_idx);
-    v_intensity[v_idx] = std::max(0.f, n * u_lightDir * -1.f);
-    return utils::m2v(u_mvp * utils::v2m(v));
-  }
-  bool fragment(Vec3f bc, Color &color) override {
-    double intensity = v_intensity * bc;
-    if (intensity > .85)
-      intensity = 1;
-    else if (intensity > .60)
-      intensity = .80;
-    else if (intensity > .45)
-      intensity = .60;
-    else if (intensity > .30)
-      intensity = .45;
-    else if (intensity > .15)
-      intensity = .30;
-    else
-      intensity = 0;
-    color = Color(unsigned char(intensity * 255),
-                  unsigned char((1 - intensity) * intensity * 200),
-                  unsigned char(intensity * 255));
-    return true;
-  }
-};
-
-struct DeepPurpleFlatShader : IShader {
-  DeepPurpleFlatShader() { _name = "DeepPurpleFlatShader"; }
-  // vertex data
-  const Model *model;
-
-  // varying
-  float v_intensity;
-
-  // uniform variables
-  Vec3f u_lightDir;
-  // Color u_color;
-  Matrix u_mvp; // model view projection matrix
-
-  Vec3f vertex(int face_idx, int v_idx) override {
-    const auto &f = model->face(face_idx);
-    auto v = model->vert(f[v_idx].v_idx);
-    auto n = model->face_normal(face_idx);
-    v_intensity = n * u_lightDir;
-    return utils::m2v(u_mvp * utils::v2m(v));
-  }
-  bool fragment(Vec3f, Color &color) override {
-    color = Color(unsigned char(v_intensity * 255),
-                  unsigned char((1 - v_intensity) * v_intensity * 255),
-                  unsigned char(v_intensity * 255));
-    // color = Color(
-    //    unsigned char(
-    //        ((sin(utils::map(v_intensity, 0, 1, 0, 6.28f)) + 1) / .5) * 255),
-    //    unsigned char((1 - v_intensity) * 255),
-    //    unsigned char(v_intensity * 255), 255);
-    return true;
-  }
-};
-
-struct DiffuseTextureShader : IShader {
-  DiffuseTextureShader() { _name = "DiffuseTextureShader"; }
+struct NormalMappingShader : IShader {
+  NormalMappingShader() { _name = "NormalMappingDiffuseShader"; }
   // vertex data
   const Model *model;
 
   // varying variables
-  std::array<Vec2f, 3> uv;
+  std::array<Vec2f, 3> v_uv;
 
   // uniform variables
   Vec3f u_lightDir;
   const Image *u_diffuse;
-  Matrix u_mvp; // model view projection matrix
+  const Image *u_normal;
+  Matrix u_vmvp; // viewport model view projection matrix
+  Matrix u_mvp;    // model view projection matrix
+  Matrix u_mvp_IT; // model view projection matrix inverse transpose  
+  void setup() override{
+    u_lightDir = utils::m2v(u_mvp * utils::v2m(u_lightDir * -1.f)).normalize();
+  }
 
   Vec3f vertex(int face_idx, int v_idx) override {
     const auto &f = model->face(face_idx);
     auto v = model->vert(f[v_idx].v_idx);
-    uv[v_idx] = model->tex(f[v_idx].t_idx);
-    return utils::m2v(u_mvp * utils::v2m(v));
+    v_uv[v_idx] = model->tex(f[v_idx].t_idx);
+    return utils::m2v(u_vmvp * utils::v2m(v));
   }
   bool fragment(Vec3f bc, Color &color) override {
-    Vec2f t;
+    Vec2f uv;
     for (int i = 0; i < 3; i++) {
-      t = t + (uv[i] * bc[i]);
+      uv = uv + (v_uv[i] * bc[i]);
     }
-    color = u_diffuse->get(int(t.x() * u_diffuse->get_width()),
-                           int((1 - t.y()) * u_diffuse->get_height()));
+    Color nc = utils::get_uv(*u_normal, uv);
+    Vec3f n(nc.red(), nc.green(), nc.blue());
+    n.normalize();
+    n = utils::m2v( u_mvp_IT * utils::v2m(n) ).normalize();
+    float intensity = std::max(0.f, n * u_lightDir);
+
+    color = utils::get_uv(*u_diffuse, uv) * intensity;
+    //color = Colors::White * intensity; // Without diffuse texture
+
     return true;
   }
 };
 
 int main() {
   Image texture(GetDiffuseTexture(MODEL));
+  Image normal_texture(GetNormalTexture(MODEL));
   Model model(GetObjPath(MODEL));
 
-  Vec3f eye(10, 5, 10);
+  Vec3f eye(0, 0, 10);
   Vec3f center(0, 0, 0);
   Vec3f up(0, 1, 0);
   Vec3f view_dir = (eye - center).normalize();
@@ -262,78 +108,27 @@ int main() {
   std::vector<std::unique_ptr<IShader>> shaders;
 
   {
-    // No shader
-    auto shader = std::make_unique<NoShader>();
-    shader->model = &model;
-    shader->u_color = Colors::White;
-    shader->u_mvp = ViewportProjectionView;
-    // shaders.emplace_back(shader.release());
-  }
-
-  {
-    // Wireframe shader
-    auto shader = std::make_unique<WireframeShader>();
-    shader->model = &model;
-    shader->u_color = Colors::White;
-    shader->u_mvp = ViewportProjectionView;
-    // shaders.emplace_back(shader.release());
-  }
-
-  {
-    // Flat Shader
-    auto shader = std::make_unique<FlatShader>();
-    shader->model = &model;
-    model.generateFaceNormals();
-    shader->u_lightDir = Vec3f(0, 0, -1).normalize();
-    shader->u_color = Colors::White;
-    shader->u_mvp = ViewportProjectionView;
-    shaders.emplace_back(shader.release());
-  }
-
-  {
-    // Flat Shader
-    auto shader = std::make_unique<DeepPurpleFlatShader>();
-    shader->model = &model;
-    model.generateFaceNormals();
-    shader->u_lightDir = Vec3f(0, 0, -1).normalize();
-    shader->u_mvp = ViewportProjectionView;
-    shaders.emplace_back(shader.release());
-  }
-
-  {
     // GouraudShader
     auto shader = std::make_unique<GouraudShader>();
     shader->model = &model;
     shader->u_lightDir = Vec3f(0, 0, -1).normalize();
     shader->u_color = Colors::White;
     shader->u_mvp = ViewportProjectionView;
-    shaders.emplace_back(shader.release());
-  }
-
-  {
-    // SmoothShader
-    auto shader = std::make_unique<SmoothShader>();
-    shader->model = &model;
-    shader->u_lightDir = Vec3f(0, 0, -1).normalize();
-    shader->u_mvp = ViewportProjectionView;
-    shaders.emplace_back(shader.release());
-  }
-  {
-    // ToonShader
-    auto shader = std::make_unique<ToonShader>();
-    shader->model = &model;
-    shader->u_lightDir = Vec3f(0, 0, -1).normalize();
-    shader->u_mvp = ViewportProjectionView;
-    shaders.emplace_back(shader.release());
+    //shaders.emplace_back(shader.release());
   }
 
   {
     // Diffuse Texture
-    auto shader = std::make_unique<DiffuseTextureShader>();
+    auto shader = std::make_unique<NormalMappingShader>();
     shader->model = &model;
     shader->u_diffuse = &texture;
-    shader->u_mvp = ViewportProjectionView;
-    // shaders.emplace_back(shader.release());
+    shader->u_normal = &normal_texture;
+    shader->u_vmvp = ViewportProjectionView;
+    shader->u_mvp = Projection * ModelView;
+    shader->u_mvp_IT = (Projection * ModelView).inverse().transpose();
+    shader->u_lightDir = Vec3f(0, 0, -1).normalize();
+    shader->setup();
+    shaders.emplace_back(shader.release());
   }
 
   for (auto &shader : shaders) {
