8cb5f1fb1493de633f497f570ba336973ffd0e77
Nitish Puri <npuri1903@gmail.com>
2019-09-29 04:52:51 +0530

    file conversion.

--- examples/file_conversion.cpp
+++ examples/file_conversion.cpp
@@ -2,10 +2,10 @@
 
 int main() {
   std::string base_path = "D:/tree/rendering/tinyrenderer/obj/";
-  std::string filename = "diablo3_pose/diablo3_pose_diffuse";
+  std::string filename = "african_head/african_head_nm_tangent";
   std::string in_file = base_path + filename + ".tga";
-  std::string out_file = base_path + filename + ".png";
+  std::string out_file = base_path + filename + ".jpg";
 
   Image test_img(in_file);
-  test_img.write(out_file);
+  test_img.write(out_file, false);
 }
